3728fd7e2513bc845e26cb698a725a2697853afe
whatisvlade <vladislavzyablov1997@gmail.com>
2025-08-07 16:21:04 +0300

    Update Redirect on 403 Forbidden.user.js

--- static_base/Redirect on 403 Forbidden.user.js
+++ static_base/Redirect on 403 Forbidden.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Заменяет текст 403 Forbidden и перенаправляет на главную страницу
-// @match        https://belarus.blsspainglobal.com/*
+// @match        https://appointment.thespainvisa.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +16,7 @@
 
         // Перенаправляем на главную страницу через 1 секунду
         setTimeout(() => {
-            window.location.href = 'https://belarus.blsspainglobal.com/Global/account/Login?returnUrl=%2FGlobal%2Fappointment%2Fnewappointment&err=HU7zqU0yCxX3GNnx4emgb8d%2FwA73yBclF%2B5Wi%2B0CSYM%3D'; // Перенаправление на главную
+            window.location.href = 'https://appointment.thespainvisa.com/Global/account/Login'; // Перенаправление на главную
         }, 2000); // 1000 мс = 1 секунда
     }
 })();
