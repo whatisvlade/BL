ccf9127ddbd0df66fde1a1893b7dd0de51536474
whatisvlade <vladislavzyablov1997@gmail.com>
2025-08-08 00:29:18 +0300

    Update Redirect on 403 Forbidden.user.js

--- static_base/Redirect on 403 Forbidden.user.js
+++ static_base/Redirect on 403 Forbidden.user.js
@@ -12,11 +12,11 @@
 
     // Проверяем, содержит ли страница текст "403 Forbidden"
     if (document.body.innerHTML.includes("403 Forbidden")) {
-        document.body.innerHTML = document.body.innerHTML.replace("403 Forbidden", "НЕТ ДОСТУПА К СТРАНИЦЕ");
+        document.body.innerHTML = document.body.innerHTML.replace("403 Forbidden", "NO ACCESS TO THE PAGE. THE IP ADDRESS MAY NOT MATCH THE REQUIRED LOCATION");
 
         // Перенаправляем на главную страницу через 1 секунду
         setTimeout(() => {
             window.location.href = 'https://appointment.thespainvisa.com/Global/account/Login'; // Перенаправление на главную
-        }, 2000); // 1000 мс = 1 секунда
+        }, 5000); // 1000 мс = 1 секунда
     }
 })();
