fc68f76a627d82eacbd701aa1d39927ee8774f17
whatisvlade <vladislavzyablov1997@gmail.com>
2025-10-20 22:29:24 +0300

    Update visa_type_selector.user.js

--- visa_type/visa_type_selector.user.js
+++ visa_type/visa_type_selector.user.js
@@ -193,7 +193,7 @@
 
             showMessage(`Категория выбрана: ${chosenCategory}. Отправляем форму...`);
 
-            await delay(2000);
+            await delay(1300);
             const $btnSubmitRaw = await waitForVisible('#btnSubmit');
             ($btnSubmitRaw[0] || $btnSubmitRaw).click();
 
